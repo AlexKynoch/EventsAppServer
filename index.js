e346fa944f976432793f8de889ecb8395396b3fd
AlexKynoch <67015973+AlexKynoch@users.noreply.github.com>
2021-09-16 19:58:41 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
+const cors = require("cors");
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());  //parse incomming requests to json
 
+app.use(cors({
+    origin: "http://localhost:3001",
+})
+);  //execute this function for any incoming requests - add header to incoming
+
 app.listen(5000, () => console.log("Server started on port 5000"));
 
 
-// set up routers
+// set up router
 
 app.use("/event", require("./routers/eventsRouter"));  // http://localhost:5000/events/test
 
